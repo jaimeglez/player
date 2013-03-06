c92e61242754091d818316e8dde3546771e09c84
Narciso Guillen <narciso.guillen@tangosource.com>
2013-03-06 16:05:36 -0600

    Bootstrap

---
+++ app.js
@@ -0,0 +1,47 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+var albums = require('./albums');
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+app.get('/albums', function(req, res){
+  res.send(albums);
+});
+
+app.get('/album', function(req, res){
+  res.send({
+    title: 'Killing the trainees',
+    artist: 'Federico',
+    tracks: []
+  });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
